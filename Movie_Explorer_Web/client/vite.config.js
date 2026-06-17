9405397bef40e317b5f124b03389089fc9835b0b
Nipun Induwara <nipuninduwara.edu@gmail.com>
2026-06-17 23:52:41 +0530

    Update vite.config.js

--- Movie_Explorer_Web/client/vite.config.js
+++ Movie_Explorer_Web/client/vite.config.js
@@ -1,7 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Movie Explorer',
+        short_name: 'Movies',
+        description: 'Discover movies and manage watchlist',
+        theme_color: '#000000',
+        background_color: '#000000',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/pwa-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/pwa-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
